e4efe28b64ef1245ea003dcde70658e1b6b53c26
angenue <angelica1nuesi@gmail.com>
2023-12-14 16:20:19 -0500

    Initial commit

---
+++ src/main/java/com/plantcare/plantcareassistant/SmartPlantCareAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.plantcare.plantcareassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPlantCareAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPlantCareAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plantcare/plantcareassistant/SmartPlantCareAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantcare.plantcareassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPlantCareAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
