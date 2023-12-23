917fa53d9173041202822c496d61adf10c8aa155
angenue <angelica1nuesi@gmail.com>
2023-12-23 14:55:33 -0500

    authentication testing

--- src/test/java/com/plantcare/plantcareassistant/controller/AuthControllerTest.java
+++ src/test/java/com/plantcare/plantcareassistant/controller/AuthControllerTest.java
@@ -1,4 +1,31 @@
 package com.plantcare.plantcareassistant.controller;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = AuthController.class,
+        excludeAutoConfiguration = SecurityAutoConfiguration.class)
 public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    public void whenLogin_thenReturnSuccessMessage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/auth/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Logged in successfully"));
+    }
 }
+
