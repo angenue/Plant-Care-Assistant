f389ce6aa260c330a52fec251ac33bd36910f7a6
angenue <angelica1nuesi@gmail.com>
2023-12-29 13:33:29 -0500

    allow cleartext traffic for ip to use http

--- AndroidApp/app/src/main/java/com/example/plantcare/util/RetrofitService.kt
+++ AndroidApp/app/src/main/java/com/example/plantcare/util/RetrofitService.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitService {
-    private const val BASE_URL = "http://localhost:8080/"
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
