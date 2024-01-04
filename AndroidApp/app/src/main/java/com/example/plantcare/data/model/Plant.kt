3be9465269cc35206c3521199bb034388aaee13f
angenue <angelica1nuesi@gmail.com>
2024-01-04 18:17:48 -0500

    "ensure that the depthWaterRequirement field in Plant data class can handle both JSON objects and arrays"

--- AndroidApp/app/src/main/java/com/example/plantcare/data/model/Plant.kt
+++ AndroidApp/app/src/main/java/com/example/plantcare/data/model/Plant.kt
@@ -2,33 +2,24 @@ package com.example.plantcare.data.model
 
 import com.google.gson.annotations.SerializedName
 
+import com.google.gson.*
+import com.google.gson.annotations.JsonAdapter
+import java.lang.reflect.Type
+
 data class Plant(
     val id: String,
-
-    @SerializedName("common_name")
-    val commonName: String,
-
-    @SerializedName("scientific_name")
-    val scientificName: List<String>,
-
+    @SerializedName("common_name") val commonName: String,
+    @SerializedName("scientific_name") val scientificName: List<String>,
     val type: String,
     val indoor: Boolean,
     val watering: String,
-
-    @SerializedName("default_image")
-    val defaultImage: DefaultImage,
-
-    @SerializedName("care_level")
-    val careLevel: String,
+    @SerializedName("default_image") val defaultImage: DefaultImage,
+    @SerializedName("care_level") val careLevel: String,
     val description: String,
     val sunlight: Set<String>,
-
-    @SerializedName("depth_water_requirement")
-    val depthWaterRequirement: List<WaterRequirement>?,
-
-    @SerializedName("watering_general_benchmark")
-    val wateringTime: WateringTime?,
-
+    @JsonAdapter(DepthWaterRequirementDeserializer::class)
+    @SerializedName("depth_water_requirement") val depthWaterRequirement: List<WaterRequirement>?,
+    @SerializedName("watering_general_benchmark") val wateringTime: WateringTime?,
     val imageUrl: String
 )
 
@@ -38,12 +29,25 @@ data class WaterRequirement(
 )
 
 data class WateringTime(
-    val value: String,  // num of days
-    val unit: String //hours, weeks, days, etc
+    val value: String,
+    val unit: String
 )
 
 data class DefaultImage(
-    @SerializedName("medium_url")
-    val mediumUrl: String
+    @SerializedName("medium_url") val mediumUrl: String
 )
 
+class DepthWaterRequirementDeserializer : JsonDeserializer<List<WaterRequirement>> {
+    override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): List<WaterRequirement> {
+        val result = mutableListOf<WaterRequirement>()
+        if (json.isJsonArray) {
+            val array = json.asJsonArray
+            for (element in array) {
+                result.add(context.deserialize(element, WaterRequirement::class.java))
+            }
+        } else if (json.isJsonObject) {
+            result.add(context.deserialize(json, WaterRequirement::class.java))
+        }
+        return result
+    }
+}
